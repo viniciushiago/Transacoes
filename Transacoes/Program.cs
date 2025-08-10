cdb316a761d7cf0996a56f1697e47c29671d81aa
viniciushiagorosa <viniciushiagor@gmail.com>
2025-08-10 17:36:56 -0300

    feat: adicionado docker

--- Transacoes/Program.cs
+++ Transacoes/Program.cs
@@ -23,14 +23,9 @@ builder.Services.AddScoped<ITransacaoService, TransacaoService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
